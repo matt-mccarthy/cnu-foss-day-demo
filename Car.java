577f24ed832cec4a90e4e7ae342295ae8e8dce80
Matt McCarthy <matt.mccarthy@protonmail.ch>
2016-02-28 14:24:16 -0500

    Initial commit and added Car.java

---
+++ Car.java
@@ -0,0 +1,134 @@
+public class Car
+{
+	private String make;
+	private String model;
+	private String licensePlate;
+	private String vin;
+	private String state;
+	private int year;
+	private int numDoors;
+	private int numSeats;
+	private int mileage;
+	private int numCollisions;
+
+	public Car()
+	{
+		// TODO: Initialize everything to 0 or ""
+	}
+
+	public String getMake()
+	{
+		// TODO: get make
+		return null;
+	}
+
+	public void setMake(String in)
+	{
+		// TODO: set make
+	}
+
+	public String getModel()
+	{
+		// TODO: get model
+		return null;
+	}
+
+	public void setModel(String in)
+	{
+		// TODO: set model
+	}
+
+	public String getLicensePlate()
+	{
+		// TODO: get the license plate number
+		return null;
+	}
+
+	public void setLicensePlate(String in)
+	{
+		// TODO: set the license plate number
+	}
+
+	public String getVIN()
+	{
+		// TODO: get the VIN
+		return null;
+	}
+
+	public void setVIN(String in)
+	{
+		// TODO: set the VIN
+	}
+
+	public String getState()
+	{
+		// TODO: get the state
+		return null;
+	}
+
+	public void setState(String in)
+	{
+		// TODO: set the state
+	}
+
+	public int getYear()
+	{
+		// TODO: get the year
+		return -1;
+	}
+
+	public void setYear(int in)
+	{
+		// TODO: set the year
+	}
+
+	public int getNumDoors()
+	{
+		// TODO: get the number of doors
+		return -1;
+	}
+
+	public void setNumDoors(int in)
+	{
+		// TODO: set the number of doors
+	}
+
+	public int getNumSeats()
+	{
+		// TODO: get the number of seats
+		return -1;
+	}
+
+	public void setNumSeats()
+	{
+		// TODO: set the number of seats
+	}
+
+	public int getMileage()
+	{
+		// TODO: get the mileage
+		return -1;
+	}
+
+	public void setMileage(int in)
+	{
+		// TODO: set the mileage
+	}
+
+	public int getNumCollisions()
+	{
+		// TODO: get the number of collisions
+		return -1;
+	}
+
+	public void setNumCollisions()
+	{
+		// TODO: set the number of collisions
+	}
+
+	public int incrementCollisions()
+	{
+		// TODO: increment the number of collisions and return it
+		return (-2)++;
+	}
+}
